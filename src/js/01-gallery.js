31f21c4dae7a927a2ec2614a05dc15127dbf3b71
EvgenyShokotko <103266879+EvgenyShokotko@users.noreply.github.com>
2022-09-06 14:20:43 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,27 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log(555)
+
+const galleryConteiner = document.querySelector('.gallery');
+console.log(galleryConteiner);
+
+const makeGalleryElements = galleryItems.map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image"src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+      </div>`
+).join('');
+console.log(makeGalleryElements)
+
+galleryConteiner.insertAdjacentHTML("afterbegin", makeGalleryElements);
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.options.captionsData = 'alt';
+gallery.options.captionDelay = 250;
